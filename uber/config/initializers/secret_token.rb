72ead056899b457875f55cfea89b0750cfb0b086
Bruce <linxinda1988@gmail.com>
2014-07-12 21:37:42 -0700

    initial commit

---
+++ uber/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uber::Application.config.secret_token = 'b7bfeb52b744f71603f3ea8f0df1e06f4da6dbdf23f8dff59ad6aff019e837795e513e397cec2e65e0a4af3fc2bca20f69c6ede302eea1bb13db89df05db1240'
